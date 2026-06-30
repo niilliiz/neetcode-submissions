aa291155fb5edb3b3636b7965ea9408e3731d06e
Niloufar Vatanara <63535114+niilliiz@users.noreply.github.com>
2026-06-30 12:25:40 +0330

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    isAlphaNumeric(char) {
+        return /^[a-z0-9]$/i.test(char);
+    }
+
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            while (l < r && !this.isAlphaNumeric(s[l])) {
+                l++;
+            }
+
+            while (l < r && !this.isAlphaNumeric(s[r])) {
+                r--;
+            }
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
